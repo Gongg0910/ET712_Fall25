1209e80ba3e1e17ace756a959c4aa250c2dc1be0
Gongg0910 <chefgonzalonyc@gmail.com>
2025-11-06 20:43:18 +0000

    react props

---
+++ lab12_props/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import User from './comment';
+import User_feedback from './User_feedback'
+
+// import images
+import avatar1 from './images/avatar_batman.png'
+import avatar2 from './images/avatar_student.png'
+import avatar3 from './images/avatar_support.png'
+
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign: "center"}}>Lab 12, React props by Gonzalo Guerra</h1>
+    <section className='container'>
+      {/** user 1 */}
+      <User image = {avatar1} name = "Batman" date = "04/23/2025" msg = "I am Batman"/>
+
+      {/** user 2 */}
+      <User image = {avatar2} name = "Daisy" date = "01/20/2025" msg = "Where is the bus stop?"/>
+
+      {/** user 3 */}
+      <User image = {avatar3} name = "Peter" date = "12/13/2024" msg = "I need help!" />
+    </section>
+
+
+    <section className='container'>
+      {/** user 1 */}
+      <User_feedback username = "Batman">  <User image = {avatar1} name = "Batman" date = "04/23/2025" msg = "I am Batman"/></User_feedback>
+
+      {/** user 2 */}
+      <User_feedback username = "Daisy">  <User image = {avatar2} name = "Batman" date = "01/20/2025" msg = "Where is the bus stop?"/></User_feedback>
+
+      {/** user 3 */}
+      <User_feedback>  <User image = {avatar3} name = "Batman" date = "12/13/2024" msg = "I need help!"/></User_feedback>
+
+    </section>
+
+    </>
+  );
+}
+
+export default App;
